8d49a0e0c05a7e3c4a67f19ad03b4ae7751481f3
Baibhav <paudelbaibhav0@gmail.com>
2022-03-27 19:06:30 +0545

    python calculator

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1 =float ( input ("enter first number :"))
+op = input ("enter your operator :")
+num2 =float ( input ("enter secound  number :"))
+if op == "+":
+ print(num1 + num2)
+elif op == "-":
+ print(num1 - num2)
+elif op == "*":
+ print(num1 * num2)
+elif op == "/":
+ print(num1 / num2)
+else:
+ print ("invalid operation")
+
